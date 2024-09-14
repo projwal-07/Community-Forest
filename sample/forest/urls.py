e994e5f1891e14c9e433384732b0e39a1e2db5fa
Projwal Adhikari <projwaladhkikari786@gmail.com>
2024-09-14 20:26:31 +0545

    Fetching from database in karya samiti about page completed

--- sample/forest/urls.py
+++ sample/forest/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import path
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('karyasamiti/',views.karyasamiti,name='karyasamiti'),
@@ -9,3 +11,5 @@ urlpatterns = [
     path('tolesamiti/',views.tolesamiti,name='tolesamiti'),
     path('chetriyasamiti/',views.chetriyasamiti,name='chetriyasamiti'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
